6f7cce990f5f203a6b89d647a0b725b95034602b
Alp Tahta <alp.tahta@protonmail.com>
2022-08-24 01:21:58 +0300

    Add Flag for init function to get user input for microservice name

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,20 @@
 package main
+
+import "flag"
+
+// InitPath is the path for where to create microservice.
+const InitPath = "../"
+
+// DefaultServiceName is the default value for service name if service name is not provided by user
+const DefaultServiceName = "default"
+
+// ServiceName is the service name given by the user.
+var ServiceName string
+
+func init() {
+	flag.StringVar(&ServiceName, "service", DefaultServiceName, "service name")
+}
+
+func main() {
+
+}
